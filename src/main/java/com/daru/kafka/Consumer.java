8eadb09382225426d5dfe239629dbeae7dcb886f
Michael Malto <michaeldm233@gmail.com>
2021-04-09 16:27:03 +0100

    added consumer, producer with callback and keys

---
+++ src/main/java/com/daru/kafka/Consumer.java
@@ -0,0 +1,60 @@
+package com.daru.kafka;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Properties;
+
+public class Consumer {
+
+    public static void main(String[] args) {
+
+        Logger LOGGER = LoggerFactory.getLogger(Consumer.class);
+        String bootstrapServer =  "127.0.0.1:9092";
+        String groupID = "first_app";
+        String topic = "first_topic";
+
+
+        // create consumer configs
+        Properties properties = new Properties();
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServer);
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupID);
+        // earliest -  read from very start of topic
+        // latest - read from only the new messages
+        // none - throw error if no offsets being saved
+        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+        // create consumer
+        KafkaConsumer<String, String> consumer =
+                new KafkaConsumer<String, String>(properties);
+
+        // subscribe consumer to topic
+        // collection.singleton is sub to onlu one topic
+        // consumer.subscribe(Collections.singleton("first_topic"));
+        consumer.subscribe(Arrays.asList("first_topic"));
+
+
+        // poll for new data
+        while (true){
+            ConsumerRecords<String, String> records
+                    = consumer.poll(Duration.ofMillis(100));
+
+            for(ConsumerRecord<String, String> record : records){
+                LOGGER.info("Key: " + record.key() + ", Value: " + record.value());
+                LOGGER.info("Partition: " + record.partition() + ", Offset: " + record.offset());
+            }
+        }
+
+    }
+
+}
---
+++ src/main/java/com/daru/kafka/ProducerWithCallback.java
@@ -0,0 +1,63 @@
+package com.daru.kafka;
+
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
+public class ProducerWithCallback {
+
+    public static void main(String[] args) {
+
+        Logger LOGGER = LoggerFactory.getLogger(ProducerWithCallback.class);
+
+        String bootstrapServer =  "127.0.0.1:9092";
+
+        // producer propoerties
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServer);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        // create producer
+        // Key String, Value String
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
+
+        // create producer record
+        ProducerRecord<String, String> record =
+                new ProducerRecord<String, String>("first_topic", "hello_world");
+
+
+        for (int i = 0; i < 10; i++) {
+
+        }
+
+        // send data - asynchronous
+        producer.send(record, new Callback() {
+            @Override
+            public void onCompletion(RecordMetadata recordMetadata, Exception e) {
+                // executes every time a record is successfully sent or exception is thrown
+                if (e == null){
+                    // record was sent successfully
+                    LOGGER.info("Received new metadata: \n" +
+                            "Topic: " + recordMetadata.topic() + "\n" +
+                            "Partition: " + recordMetadata.partition() + "\n" +
+                            "Offset: " + recordMetadata.offset() + "\n" +
+                            "Timestamp: " + recordMetadata.timestamp());
+                } else {
+                    LOGGER.error("Error while producing: ", e);
+                }
+
+            }
+        });
+
+        // flush data
+        producer.flush();
+        // flush and close producer
+        producer.close();
+
+    }
+
+}
